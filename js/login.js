5346eec5dcfdda06980a45ef78158d226a41a4fc
Fadlan966 <fadlangaming42@gmail.com>
2023-11-25 17:18:56 +0700

    first commit

---
+++ js/login.js
@@ -0,0 +1,20 @@
+// button = document.getElementById("Login");
+// button.onsubmit = function(){
+// alert("berhasil Login");
+// }
+button = document.getElementById('submit');
+button.addEventListener('click' , function () {
+  username = document.getElementById("username");
+  password = document.getElementById("password");
+
+  
+    window.location = "/html/index.html";
+
+
+});
+// button = document.getElementById("Login");
+// button.addEventListener('click', function(){
+//     alert('yes');
+
+//     window.location = 'Welcome.html';
+// });
